44c18a36c5c5aaddcf63a0c7bd81bf06b577f5c6
Abdulaziz Hamid <abdulaziz5536h@gmail.com>
2026-03-28 23:02:46 +0300

    update

--- frontend/src/services/weatherService.ts
+++ frontend/src/services/weatherService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = "2d56abc0e7c61bf5d885f0b9984cc83a";
+const API_KEY = import.meta.env.VITE_API_KEY as string;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
 
 export const getWeather = async (city: string) => {
